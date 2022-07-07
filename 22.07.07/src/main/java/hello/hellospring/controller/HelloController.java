fda133cdee0391b414dc353574b7d7af975227f9
KangSuKyung <kskksk1122@naver.com>
2022-07-07 22:53:05 +0900

    (인프런)스프링 입문_MVC와 템플릿 엔진

---
+++ 22.07.07/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("hello")
+	public String hello(Model model) {
+		model.addAttribute("data", "hello!!!");	//data는 model을 resources/templates/hello.html에 넘김
+		return "hello"; // resources/templates/hello.html을 찾아 가라는 것
+	}
+	
+	@GetMapping("hello-mvc")
+	public String helloMVC(@RequestParam("name") String name, Model model) { /* 데이터 넘겨주기 필수!! */
+		model.addAttribute("name", name);
+		return "hello-templete";
+	}
+}
